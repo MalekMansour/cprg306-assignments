fee11eae3b71bd206850364b6530051e70b941a8
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-09-10 00:35:14 -0600

    update

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -1,10 +1,15 @@
+// /app/page.js
+
+import React from 'react';
+import Link from 'next/link'; // Make sure this is imported for client-side navigation
+
 export default function Page() {
-  return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">
-        <a>Go to Week 2 Assignments</a>
-      </Link>
-    </main>
-  );
+    return (
+      <main>
+        <h1>CPRG 306: Web Development 2 - Assignments</h1>
+        <Link href="/week-2">
+          <a>Go to Week 2 Assignments</a> {/* Link to the week-2 page */}
+        </Link>
+      </main>
+    );
 }
