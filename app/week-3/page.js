ae9e5e9f5001a6d60d1737c58f7a5aa29ba492a7
Malek Mansour <malek24mansour@gmail.com>
2024-09-11 17:29:21 -0600

    update

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,7 +2,7 @@ const Page = () => {
     return (
       <main className="min-h-screen bg-gray-50 p-6">
         <h1 className="text-3xl font-bold mb-4 text-center text-gray-900">Shopping List</h1>
-        <ItemList />
+        <item-list/>
       </main>
     );
   };
