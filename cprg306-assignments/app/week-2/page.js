b151dbdef2dc0b8f39576a2b0a40a31ada739240
Malek Mansour <malek24mansour@gmail.com>
2024-09-11 12:28:03 -0600

    commit

--- cprg306-assignments/app/week-2/page.js
+++ cprg306-assignments/app/week-2/page.js
@@ -1,8 +1,9 @@
-import StudentInfo from './student-info.js ';
+import StudentInfo from './student-info.js';
 export default function Page() {
     return (
       <main>
         <h1>Shopping List</h1>
+        <StudentInfo /> 
       </main>
     );
   }
