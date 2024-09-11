9481607a7704b8b73e5d94ad60ab79e365bf3c99
Malek Mansour <malek24mansour@gmail.com>
2024-09-11 12:32:11 -0600

    Commit

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -1,15 +1,13 @@
 // /app/page.js
 
 import React from 'react';
-import Link from 'next/link';
+import Link from 'next/link'; // Assuming you're using Next.js
 
 export default function Page() {
     return (
       <main>
         <h1>CPRG 306: Web Development 2 - Assignments</h1>
-        <Link href="/week-2">
-          <a>Go to Week 2 Assignments</a>
-        </Link>
+          <a href="/week-2">Go to Week 2 Assignments</a>
       </main>
     );
 }
