02018af32ca097acaa4d925cb0cd23c43f9fb197
Malek Mansour <malek24mansour@gmail.com>
2024-09-11 17:12:22 -0600

    page.js updated

---
+++ app/week-3/page.js
@@ -0,0 +1,10 @@
+const Page = () => {
+    return (
+      <main className="min-h-screen bg-gray-50 p-6">
+        <h1 className="text-3xl font-bold mb-4 text-center text-gray-900">Shopping List</h1>
+        <ItemList />
+      </main>
+    );
+  };
+  
+  export default Page;
