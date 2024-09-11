c20f23dbb9ead3c0348ee37b2eacca48b2d2cdde
Malek Mansour <malek24mansour@gmail.com>
2024-09-11 12:35:33 -0600

    Update

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import Link from 'next/link'; 
 
 export default function Page() {
     return (
       <main>
         <h1>CPRG 306: Web Development 2 - Assignments</h1>
-          <a herf="/week-2">Week 2 Assignment</a>
+          <a href="/week-2">Week 2 Assignment</a>
       </main>
     );
 }
