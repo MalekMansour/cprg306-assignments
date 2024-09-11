fbfa8a0f289faa400a7d60a225ebc299576bfdf2
Malek Mansour <malek24mansour@gmail.com>
2024-09-11 17:42:28 -0600

    Update

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,10 +1,12 @@
+import ItemList from './item-list'; 
+
 const Page = () => {
-    return (
-      <main className="min-h-screen bg-gray-50 p-6">
-        <h1 className="text-3xl font-bold mb-4 text-center text-gray-900">Shopping List</h1>
-        <item-list/>
-      </main>
-    );
-  };
-  
-  export default Page;
+  return (
+    <main className="min-h-screen bg-gray-50 p-6">
+      <h1 className="text-3xl font-bold mb-4 text-center text-gray-900">Shopping List</h1>
+      <ItemList/> 
+    </main>
+  );
+};
+
+export default Page;
