fe55610530c2f7b4ed62e093bb1ca4953258991f
Malek Mansour <malek24mansour@gmail.com>
2024-09-11 20:22:59 -0600

    page.js Updated

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ export default function Page() {
       <main>
         <h1>CPRG 306: Web Development 2 - Assignments</h1>
           <a href="/week-2">Week 2 Assignment</a>
+          <a href="/week-2">Week 3 Assignment</a>
       </main>
     );
 }
