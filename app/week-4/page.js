350a89603291eb685dffdfb5c33008e57547691a
Malek Mansour <malek24mansour@gmail.com>
2024-09-14 19:21:04 -0600

    Update page.js

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,8 +2,8 @@ import NewItem from './new-item';
 
 const Page = () => {
   return (
-    <div>
-      <h1>Week 4 - New Item</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Week 4 - New Item</h1>
       <NewItem />
     </div>
   );
