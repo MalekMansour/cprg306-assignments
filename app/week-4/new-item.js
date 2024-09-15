d062f8fe1fb05d8507b43d395d9acc635d7dd7a4
Malek Mansour <malek24mansour@gmail.com>
2024-09-14 19:18:03 -0600

    Update new-item.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,29 +1,31 @@
-"use client";  
-
 import React, { useState } from 'react';
 
-const NewItem = () => {
-    const [quantity, setQuantity] = useState(1);  
+const QuantityComponent = () => {
+  const [quantity, setQuantity] = useState(1); 
 
-    const increment = () => {
-        if (quantity < 20) {
-            setQuantity(quantity + 1);  
-        }
-    };
+  const increment = () => {
+    if (quantity < 20) {
+      setQuantity(quantity + 1); 
+    }
+  };
 
-    const decrement = () => {
-        if (quantity > 1) {
-            setQuantity(quantity - 1);  
-        }
-    };
+  const decrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1); 
+    }
+  };
 
-    return (
-        <div>
-            <h2>Quantity: {quantity}</h2>
-            <button onClick={increment}>+</button>
-            <button onClick={decrement}>-</button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Quantity: {quantity}</h1>
+      <button onClick={decrement} disabled={quantity === 1}>
+        Decrement
+      </button>
+      <button onClick={increment} disabled={quantity === 20}>
+        Increment
+      </button>
+    </div>
+  );
 };
 
-export default NewItem;
+export default QuantityComponent;
