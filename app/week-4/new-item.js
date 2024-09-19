43ffe3f30f5c58a0cde3b3fb0619e454421acecf
Malek Mansour <malek24mansour@gmail.com>
2024-09-19 00:27:14 -0600

    Update new-item.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -21,7 +21,7 @@ export default function NewItem() {
     <div className="bg-[#1E201E] text-white p-5 rounded-md flex items-center justify-between">
       <button
         onClick={decrement}
-        className={`border-2 border-blue-800 px-3 py-1 rounded-md ${
+        className={`border-2 border-blue-900 px-3 py-1 rounded-md ${
           quantity === 1 ? 'bg-[#B4B4B8] text-white cursor-not-allowed' : 'bg-blue-500 text-white'
         }`}
         disabled={quantity === 1}
