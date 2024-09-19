38e537d63cb9383327dcb0a8ca12c0c30193efbc
Malek Mansour <malek24mansour@gmail.com>
2024-09-19 00:28:59 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Page() {
           <h2><a href="/week-2">Week 2 Assignment</a></h2> 
           <h2><a href="/week-3">Week 3 Assignment</a></h2> 
           <h2><a href="/week-4">Week 4 Assignment</a></h2> 
+          <h2><a href="/week-5">Week 5 Assignment</a></h2> 
       </main>
     );
 }
