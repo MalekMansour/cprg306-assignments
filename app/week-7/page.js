ab121c734e137c693669c095d48d79f3d726868d
Malek Mansour <malek24mansour@gmail.com>
2024-09-26 12:29:35 -0600

    Update page.js

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,11 +1,25 @@
+"use client"; 
+
+import React, { useState } from 'react';
 import ItemList from './item-list';
+import NewItem from './new-item'; 
+import itemsData from './items.json';
 
 const Page = () => {
+  const [items, setItems] = useState(itemsData);
+
+  const handleAddItem = (newItem) => {
+    setItems((prevItems) => [...prevItems, newItem]);
+  };
+
   return (
     <main className="min-h-screen bg-black-20 p-3">
       <div className="max-w-xl mx-auto">
         <h1 className="text-3xl font-bold text-center text-white-700 mb-6">Shopping List</h1>
-        <ItemList />
+
+        <NewItem onAddItem={handleAddItem} />
+
+        <ItemList items={items} />
       </div>
     </main>
   );
