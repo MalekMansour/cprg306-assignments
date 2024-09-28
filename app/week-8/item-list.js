50fcfbf1126174c8e44b6bef70f446bfc8d2bb17
Malek Mansour <malek24mansour@gmail.com>
2024-09-28 16:36:22 -0600

    Update item-list.js

--- app/week-8/item-list.js
+++ app/week-8/item-list.js
@@ -27,7 +27,7 @@ const ItemList = ({ items, onItemSelect }) => {
 
   return (
     <div>
-      <div className="flex space-x-24 mb-6">
+      <div className="flex space-x-55 mb-6">
         <button
           onClick={() => setSortBy("name")}
           className={`px-4 py-1 rounded-lg ${
