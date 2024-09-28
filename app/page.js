de5b3ee3aaf8382d9bed939857c4ef95d1ab5c1e
Malek Mansour <malek24mansour@gmail.com>
2024-09-27 18:25:53 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,6 +9,8 @@ export default function Page() {
         <h2><a href="/week-5" className="text-xl hover:text-green-500 transition">Week 5 Assignment</a></h2>
         <h2><a href="/week-6" className="text-xl hover:text-green-500 transition">Week 6 Assignment</a></h2>
         <h2><a href="/week-7" className="text-xl hover:text-green-500 transition">Week 7 Assignment</a></h2>
+        <h2><a href="/week-8" className="text-xl hover:text-green-500 transition">Week 8 Assignment</a></h2>
+
       </div>
 
       {/* Footer Section */}
