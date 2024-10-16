612bddd02a292b84469313694fdf8004a1254ed5
Malek Mansour <malek24mansour@gmail.com>
2024-10-16 15:14:33 -0600

    Update auth-context.js

--- app/week-9/_utils/auth-context.js
+++ app/week-9/_utils/auth-context.js
@@ -1,10 +1,15 @@
 "use client";
 
 import { useContext, createContext, useState, useEffect } from "react";
-import { signInWithPopup, signOut, onAuthStateChanged, GithubAuthProvider } from "firebase/auth";
+import {
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  GithubAuthProvider,
+} from "firebase/auth";
 import { auth } from "./firebase"; 
 
-const AuthContext = createContext();
+const AuthContext = createContext(); 
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -23,7 +28,7 @@ export const AuthContextProvider = ({ children }) => {
       setUser(currentUser);
     });
     return () => unsubscribe();
-  }, []);
+  }, [user]);
 
   return (
     <AuthContext.Provider value={{ user, gitHubSignIn, firebaseSignOut }}>
@@ -32,7 +37,6 @@ export const AuthContextProvider = ({ children }) => {
   );
 };
 
-
 export const useUserAuth = () => {
-  return useContext(AuthContext);
+  return useContext(AuthContext); 
 };
