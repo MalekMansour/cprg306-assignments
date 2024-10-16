586a8c6d2805c536703c529c1aaa9473c30d875d
Malek Mansour <malek24mansour@gmail.com>
2024-10-16 15:43:06 -0600

    Update page.js

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,8 +1,22 @@
-import { useUserAuth } from "./_utils/auth-context";
-const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
-console.log('useUserAuth called', context); 
-await gitHubSignIn();
-await firebaseSignOut();
-<p>
-  Welcome, {user.displayName} ({user.email})
-</p>;
+"use client";
+
+import React from "react";
+import { useUserAuth } from "./_utils/auth-context"; 
+
+const Page = () => {
+  const { user, firebaseSignOut } = useUserAuth();
+
+  if (!user) {
+    return <p>You need to be logged in to access this page.</p>; 
+  }
+
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <p>Welcome, {user.displayName} ({user.email})</p>
+      <button onClick={firebaseSignOut}>Logout</button>
+    </main>
+  );
+};
+
+export default Page;
