7909671939f937cfd7528ceb6b14b029772210b2
Malek Mansour <malek24mansour@gmail.com>
2024-10-19 15:07:01 -0600

    Add firebase.js

---
+++ app/week-10/_utils/firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAuth } from "firebase/auth"; 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiTS20Tw9cXaln1bYrGDDYVsA_IaLdo8w",
+  authDomain: "shopping-list-a6c07.firebaseapp.com",
+  projectId: "shopping-list-a6c07",
+  storageBucket: "shopping-list-a6c07.appspot.com",
+  messagingSenderId: "882065958056",
+  appId: "1:882065958056:web:0452bc13ee2b77aad58e9d",
+  measurementId: "G-BYC858S53D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+let analytics;
+
+if (typeof window !== 'undefined') {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  }).catch((error) => {
+    console.error("Analytics not supported:", error);
+  });
+}
+
+const auth = getAuth(app); 
+
+export { auth };
